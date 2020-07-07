707941906518f0beba728061d7b6077ebc031e34
bacil thalji <btbacil32@gmail.com>
2020-07-07 00:28:24 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(numbers)
     num = []
     index = 0
-    if index < numbers[index] do
+    while index < numbers.length do
       num << numbers[index] * numbers[index]
       index += 1
     end
